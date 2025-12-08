29ac2e2d587cf01ba2543127a7b352e61d6acea1
jwelgo <jkwelgo@gmail.com>
2025-12-07 19:48:30 -0500

    Tests

--- Src/pumping_logic.cpp
+++
@@ -1,6 +0,0 @@
-/** Pumping Logic File
- * @project: Automated Plant Watering System
- * @description: This file contains the logic for releasing water from
- *               the resovior. The readings of the moisture sensors will
- *               determine how much water is released. 
- */
